84864061c585481d7bd8b75bc1822f709319f81a
idotick <sanderdalangin@gmail.com>
2025-08-27 00:42:27 +0800

    added home page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,59 +1,68 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Entypo from "@expo/vector-icons/Entypo";
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { Text, View } from '@/components/Themed';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
+	name: React.ComponentProps<typeof FontAwesome>['name'];
+	color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+	return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+				headerShown: useClientOnlyValue(false, true),
+			}}>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: '',
+					tabBarStyle: { backgroundColor: '#ffa34a' },
+					headerStyle: { backgroundColor: '#151932' },
+					tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+					headerLeft: () => (
+						<View style={{ backgroundColor: 'transparent', marginLeft: 25 }}>
+							<Text style={{ fontSize: 28.6 }}>iskolar</Text>
+							<Text style={{ fontSize: 7.1 }}>by UNLAD</Text>
+						</View>
+					),
+					headerRight: () => (
+						<View style={{ marginRight: 25, backgroundColor: 'transparent', flexDirection: "row", gap: 10 }}>
+							<Link href="/modal" asChild>
+								<Pressable onPress={() => console.log("notifs")}>
+									<Entypo name='bell' size={24} color='#fff' />
+								</Pressable>
+							</Link>
+
+							<Link href="/modal" asChild>
+								<Pressable onPress={() => console.log("settings")}>
+									<Entypo name='cog' size={24} color='#fff' />
+								</Pressable>
+							</Link>
+						</View>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="two"
+				options={{
+					title: 'Tab Two',
+					tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+				}}
+			/>
+		</Tabs>
+	);
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,109 @@
-import { StyleSheet } from 'react-native';
+import Entypo from "@expo/vector-icons/Entypo";
+import { Pressable, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+function Container({ flex, children }: { flex?: Array<number>, children: any }) {
+	const flexVal = flex === undefined ? [1, 0] : flex;
+
+	flexVal.forEach(value => {
+		if (value === undefined) {
+			value = 1;
+		}
+	});
+
+	return (
+		<View style={[styles.container, {flexGrow: flexVal[0], flexShrink: flexVal[1]}]}>{children}</View>
+	);
+}
+
+function RowContainer({children}: any) {
+	return (
+		<View style={styles.row}>{children}</View>
+	);
+}
+
+export default function HomeScreen() {
+	return (
+		<View style={styles.main}>
+			<RowContainer>
+				<View style={styles.pictureContainer}>
+					<Entypo name="user" size={55} color={"#fff"}></Entypo>
+				</View>
+				<Container>
+					<Text style={{ fontSize: 12 }}>John Ivan B. Floirendo</Text>
+					<Text style={{ fontSize: 9 }}>Grade 11 - Maimai</Text>
+					<Text style={{ fontSize: 9 }}>Rhythm Gaming Block 2</Text>
+					<Text style={{ fontSize: 9 }}>WuWa Elective</Text>
+				</Container>
+			</RowContainer>
+			
+			<Container flex={[0.4]}>
+				<Text style={{ fontSize: 9 }}>CLASS SCHEDULE</Text>
+				<Text style={{ fontSize: 14.5 }}>ONGOING CLASS: CHEMISTRY 2</Text>
+				<RowContainer>
+					<Text style={{ fontSize: 7 }}>NEXT CLASS SOCIAL SCIENCE 5</Text>
+					<Text style={{ fontSize: 7 }}>12:30 - 14:20</Text>
+				</RowContainer>
+			</Container>
+
+			<Container flex={[2, 1]}>
+				<Text style={{ fontSize: 14.5 }}>ANNOUNCEMENTS</Text>
+				<View style={styles.subContainer}></View>
+			</Container>
+
+			<View style={[styles.row, { flex: 2 }]}>
+				<Container>
+					<RowContainer>
+						<Text style={{ fontSize: 14.5 }}>MENU</Text>
+						<Text style={{ fontSize: 14.5 }}>DINNER</Text>
+					</RowContainer>
+					
+					<View style={styles.subContainer}></View>
+				</Container>
+
+				<Container>
+					<Text></Text>
+					<Text style={{ fontSize: 11.9, textAlign: 'center' }}>CAFETERIA BALANCE</Text>
+				</Container>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	main: {
+		flex: 1,
+		backgroundColor: '#151932',
+		padding: 20,
+	},
+	row: {
+		backgroundColor: 'transparent',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		gap: 15,
+	},
+	container: {
+		flex: 1,
+		backgroundColor: '#ffa34a',
+		borderRadius: 17,
+		padding: 15,
+		marginBottom: 15,
+	},
+	subContainer: {
+		flex: 1,
+		backgroundColor: '#ffd9b4',
+		borderRadius: 17,
+		padding: 25,
+		marginTop: 20,
+	},
+	pictureContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 85,
+		backgroundColor: '#ffa34a',
+		borderRadius: 100,
+		padding: 10,
+	}
 });
