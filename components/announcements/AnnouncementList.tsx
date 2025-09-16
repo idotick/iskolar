4690501355e3d5b24893bd38ac70baec4b4d6804
idotick <sanderdalangin@gmail.com>
2025-09-16 21:31:58 +0800

    restyled announcements page

---
+++ components/announcements/AnnouncementList.tsx
@@ -0,0 +1,33 @@
+import { FlatList, StyleSheet, ViewProps } from 'react-native';
+
+import AnnouncementOverview, { AnnouncementData } from './AnnouncementOverview';
+
+type AnnnouncementListProps = {
+	data: AnnouncementData[],
+} & ViewProps;
+
+export default function AnnnouncementList( { data, style } : AnnnouncementListProps ) {
+	return (
+		<FlatList
+			data={data}
+			contentContainerStyle={styles.container}
+			style={[{ flex: 1 }, style]}
+			renderItem={({ item }) => (
+				<AnnouncementOverview
+					recent={item.recent}
+					date={item.date}
+					title={item.title}
+					overview={item.overview}
+				/>
+			)}
+		/>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+
+		backgroundColor: 'transparent',
+	},
+})
